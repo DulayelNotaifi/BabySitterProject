5d28ef54fd3d12daa72c1833a0c1a8babdbd8607
Lena <113691620+iLenakh@users.noreply.github.com>
2022-10-29 13:32:26 +0300

    1

---
+++ PHP_Files/jobRequest.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+
+if(isset($_POST['post_submit'])){
+
+$servername= "localhost";
+$username= "root" ;
+$password= "";
+$dbname= "381project" ;
+
+if (!$connection= mysqli_connect($servername,$username,$password)) 
+die("Connection failed: " . mysqli_connect_error());
+
+if(!$database= mysqli_select_db($connection, $dbname))
+die("Could not open database failed: " . mysqli_connect_error());
+
+if(isset($_POST['kidsname']) && isset($_POST['kidsages']) && isset($_POST['service']) && isset($_POST['form_day']) && isset($_POST['to_day']) && isset($_POST['from_time']) && isset($_POST['to_time'])){
+$kidsname = $_POST['kidsname'];
+$kidsages = $_POST['kidsages'];
+$service = $_POST['service'];
+$form_day = $_POST['form_day'];
+$to_day = $_POST['to_day'];
+$from_time = $_POST['from_time'];
+$to_time = $_POST['to_time'];
+
+$sql = "INSERT INTO 'kids' (kidsName, age, TypeOfServese, startTime, endTime, startDate, endDate ) VALUES ('$kidsname', '$kidsages','$service','$from_time','$to_time','$form_day','$to_day')";
+$query = mysqli_query($connection,$sql);
+
+if( $query ){
+    echo 'done';
+}
+else{
+    echo 'fail';
+    }
+}
+}
+
+?> 
