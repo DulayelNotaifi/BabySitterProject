c1526981ae93767d5e01cabdc8310a071f317a34
cady11 <113461945+cady11@users.noreply.github.com>
2022-11-03 20:23:23 +0300

    a

--- HTML_Files/babysitterhome.php
+++ HTML_Files/babysitterhome.php
@@ -10,6 +10,87 @@
     <link rel="stylesheet" href="../CSS_Files/menustyle.css">
     <script src="https://kit.fontawesome.com/b8b24b0649.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../CSS_Files/footer.css">
+<style>
+
+#card {
+  position: relative;
+  top: 110px;
+  width: 320px;
+  display: block;
+  margin: auto;
+  text-align: center;
+  font-family: 'Source Sans Pro', sans-serif;
+  z-index: 4;
+}
+
+#upper-side {
+  padding: 2em;
+  background-color: #8BC34A;
+  display: block;
+  color: #fff;
+  border-top-right-radius: 8px;
+  border-top-left-radius: 8px;
+}
+
+#checkmark {
+  font-weight: lighter;
+  fill: #fff;
+  margin: -3.5em auto auto 20px;
+}
+
+#status {
+  font-weight: lighter;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  font-size: 1em;
+  margin-top: -.2em;
+  margin-bottom: 0;
+}
+
+#lower-side {
+  padding: 2em 2em 5em 2em;
+  background: #fff;
+  display: block;
+  border-bottom-right-radius: 8px;
+  border-bottom-left-radius: 8px;
+}
+
+#message {
+  margin-top: -.5em;
+  color: #757575;
+  letter-spacing: 1px;
+}
+
+#contBtn {
+  position: relative;
+  top: 1.5em;
+  text-decoration: none;
+  background: #8bc34a;
+  color: #fff;
+  margin: auto;
+  padding: .8em 3em;
+  -webkit-box-shadow: 0px 15px 30px rgba(50, 50, 50, 0.21);
+  -moz-box-shadow: 0px 15px 30px rgba(50, 50, 50, 0.21);
+  box-shadow: 0px 15px 30px rgba(50, 50, 50, 0.21);
+  border-radius: 25px;
+  -webkit-transition: all .4s ease;
+		-moz-transition: all .4s ease;
+		-o-transition: all .4s ease;
+		transition: all .4s ease;
+}
+
+#contBtn:hover {
+  -webkit-box-shadow: 0px 15px 30px rgba(50, 50, 50, 0.41);
+  -moz-box-shadow: 0px 15px 30px rgba(50, 50, 50, 0.41);
+  box-shadow: 0px 15px 30px rgba(50, 50, 50, 0.41);
+  -webkit-transition: all .4s ease;
+		-moz-transition: all .4s ease;
+		-o-transition: all .4s ease;
+		transition: all .4s ease;
+}
+</style>
+
+
 </head>
 
 <body>
@@ -24,9 +105,9 @@
         <div class="tab1">
             <a href="">Manage Profile </a>
             <div class="dropdown-content">
-                <a href="viewbbystr.html">View</a>
+                <a href="viewbbystr.php">View</a>
                 <a href="babysittereditprofile.php">Edit</a>
-                <a class="last" href="deletebbystracc.html">Delete</a>
+                <a class="last" href="deletebbystracc.php">Delete</a>
             </div>
         </div>
         <div class="tab2">
@@ -55,47 +136,7 @@
             <a href="LoginPage.html">Logout</a>
         </div>
     </div>
-    <!--<div class="uppermenu">
-        <div class="tab0">
-            <a href="babysitterhome.html">Home</a>
-        </div>
-        <div class="tab1">
-            <a href="">Manage profile </a>
-            <div class="dropdown-content">
-                <a href="viewbbystr.html">View</a>
-                <a href="babysittereditprofile.html">Edit</a>
-                <a class="last" href="deletebbystracc.html">delete</a>
-            </div>
-        </div>
-        <div class="tab2">
-            <a href="#">View job request list </a>
-            <div class="dropdown-content">
-                <a href="">Send offer </a>
-                <a href="">View job request details </a>
-            </div>
-        </div>
-
-        <div class="tab3 need-more">
-            <a href="">View job </a>
-            <div class="dropdown-content">
-                <a href="">View current job </a>
-                <a href="">View previous jobs
-                </a>
-            </div>
-        </div>
-        <div class="tab4">
-            <a href="">View status of offers
-            </a>
-        </div>
-        <div class="tab4">
-            <a href="">View rates and reviews
-            </a>
-        </div>
-        
-        <div class="logout">
-            <a href="">Log out</a>
-        </div>
-    </div>-->
+    
 
     <div class="babypic">
 
