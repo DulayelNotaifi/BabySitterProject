bedc6c50c383ca108f37227177b67d6899635375
DulayelNotaifi <dulayelan09@gmail.com>
2022-11-04 02:41:46 +0300

    offersDetailsFunctionalities

---
+++ PHP_Files/deletrecord.php
@@ -0,0 +1,16 @@
+<?php 
+ include('../PHP_Files/connect_db.php');
+
+if(isset($_GET['id'])){
+
+   
+   $id=  $_GET['id'];
+   $name = $_GET['name'];
+   $sql = "DELETE FROM `offers` WHERE `RequestID` = $id AND `babySitterName` = '$name'";
+   $result = mysqli_query($connection,$sql);
+   header("Location: http://localhost/BabySitterProject/HTML_Files/OfferDetails.php?id=$id");
+   
+}
+
+
+
