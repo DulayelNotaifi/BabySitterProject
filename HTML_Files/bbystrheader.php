cbbdff8273e7a02904bc1b4b6aef70b4d8c11ded
cady11 <113461945+cady11@users.noreply.github.com>
2022-11-04 20:35:42 +0300

    a

---
+++ HTML_Files/bbystrheader.php
@@ -0,0 +1,42 @@
+<div class="topofpage">
+        <img src="thenewlogo.jpg" alt="a logo for A Watchful Eye website" class="logo-small">
+        <p class="andname">A Watchful Eye</p>
+    </div>
+    <div class="uppermenu">
+        <div class="tab0">
+            <a href="babysitterhome.html">Home</a>
+        </div>
+        <div class="tab1">
+            <a href="">Manage Profile </a>
+            <div class="dropdown-content">
+                <a href="viewbbystr.php">View</a>
+                <a href="babysittereditprofile.php">Edit</a>
+                <a class="last" href="deletebbystracc.php">Delete</a>
+            </div>
+        </div>
+        <div class="tab2">
+            <a href="viewJobRequestList.html">Job Request List </a>
+ 
+        </div>
+
+        <div class="tab3 need-more">
+            <a href="viewJobs.html">Jobs </a>
+            <div class="dropdown-content">
+                <a href="viewcurrentjobs.html">Current Job</a>
+                <a href="viewPreviousJobs.html">Previous Jobs</a>
+            </div>
+        </div>
+
+
+        <div class="tab4">
+            <a href="ViewOffersWithTheirStatus.html">My Offers with Their Status</a>
+        </div>
+        <div class="tab4">
+            <a href="ViewRate.html">Rates and Reviews
+            </a>
+        </div>
+        
+        <div class="logout">
+            <a href="LoginPage.html">Logout</a>
+        </div>
+    </div>
