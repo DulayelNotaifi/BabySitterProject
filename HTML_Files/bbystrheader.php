39d1234e633b1bdd0c5e77603f95231aaddc5e1e
Nalhumaidani <113690135+Nalhumaidani@users.noreply.github.com>
2022-11-05 12:10:56 +0300

    links changing

--- HTML_Files/bbystrheader.php
+++ HTML_Files/bbystrheader.php
@@ -15,24 +15,24 @@
             </div>
         </div>
         <div class="tab2">
-            <a href="viewJobRequestList.html">Job Request List </a>
+            <a href="viewJobRequestList.php">Job Request List </a>
  
         </div>
 
         <div class="tab3 need-more">
-            <a href="viewJobs.html">Jobs </a>
+            <a href="viewJobs.php">Jobs </a>
             <div class="dropdown-content">
-                <a href="viewcurrentjobs.html">Current Job</a>
-                <a href="viewPreviousJobs.html">Previous Jobs</a>
+                <a href="viewcurrentjobs.php">Current Job</a>
+                <a href="viewPreviousJobs.php">Previous Jobs</a>
             </div>
         </div>
 
 
         <div class="tab4">
-            <a href="ViewOffersWithTheirStatus.html">My Offers with Their Status</a>
+            <a href="ViewOffersWithTheirStatus.php">My Offers with Their Status</a>
         </div>
         <div class="tab4">
-            <a href="ViewRate.html">Rates and Reviews
+            <a href="ViewRate.php">Rates and Reviews
             </a>
         </div>
         
