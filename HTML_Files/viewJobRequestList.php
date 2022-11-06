c98c2091b5cca3e8e5ebf4c57ca97c03edc2a035
Lena <113691620+iLenakh@users.noreply.github.com>
2022-11-06 20:02:43 +0300

    26

--- HTML_Files/viewJobRequestList.php
+++ HTML_Files/viewJobRequestList.php
@@ -10,6 +10,19 @@
     <link rel="stylesheet" href="../CSS_Files/footer.css">
     <script src="https://kit.fontawesome.com/b8b24b0649.js" crossorigin="anonymous"></script>
 
+    <style>
+        html, body {
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    display: table;
+}
+
+footer {
+    
+    display: table-row;
+}
+        </style>
 </head>
 
 <body>
